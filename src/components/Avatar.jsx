fb621194a6c06d47bd928e15e8fa1d2bd639ca83
Ricardo Henrique Vinciguerra <ricardorhv.dev@gmail.com>
2023-01-26 13:54:02 -0300

    feat: creating a component Avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,14 @@
+import styles from './Avatar.module.css'
+
+export function Avatar({ src, hasBorder = true }) {
+  return (
+    <img 
+      src={src}
+      className={
+        hasBorder 
+        ? styles.avatarWithBorder 
+        : styles.avatar
+      }
+    />
+  )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img 
-          src="https://github.com/ricardorhv.png"
-          className={styles.avatar}
-        />
+        <Avatar src="https://github.com/ricardorhv.png"/>
 
         <strong>Ricardo Vinciguerra</strong>
         <span>Web Developer</span>
